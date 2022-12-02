0c696e90a43b070294e303b9f4013adc0c134702
Timur <tiabumuaz@gmail.com>
2022-12-02 08:41:29 +0500

    Добавлено возвращение на главную по клику на кнопку возврата

--- src/components/result/Result.tsx
+++ src/components/result/Result.tsx
@@ -9,7 +9,9 @@ function Result({correct}: ResultType): JSX.Element {
     <div className="result">
       <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
       <h2>Вы отгадали {correct} ответа из {questions.length}</h2>
-      <button>Попробовать снова</button>
+      <a href="/">
+        <button>Попробовать снова</button>
+      </a>
     </div>
   );
 };
